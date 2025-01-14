063afd4e3bac8b404e915793b5501c8a898be3c2
theokyr <theo.kyrr@gmail.com>
2025-01-15 01:31:51 +0200

    feat(gui): improve slider / fit appearance

--- AbleSharp.GUI/Constants/Constants.cs
+++ AbleSharp.GUI/Constants/Constants.cs
@@ -6,7 +6,7 @@ public static class Constants
     public static readonly double DEFAULT_ZOOM_LEVEL_Y = 1.0;
     public static readonly double MIN_ZOOM_X = 0.125;
     public static readonly double MAX_ZOOM_X = 50.0;
-    public static readonly double MIN_ZOOM_Y = 0.125;
-    public static readonly double MAX_ZOOM_Y = 4.0;
+    public static readonly double MIN_ZOOM_Y = 0.4;
+    public static readonly double MAX_ZOOM_Y = 2.0;
     public static readonly double ZOOM_STEP = 1.5;
 }
